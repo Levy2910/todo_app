597bba954939f4b1d73aea367b9e629f6d5cdac2
Levy2910 <levycaoesc14@gmail.com>
2022-06-16 11:52:15 +1000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,95 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useCallback, useEffect } from "react";
 
 function App() {
+  const TODO_APP_STORAGE_KEY = "TODO_APP";
+  // hooks
+  const [newAct, setNewAct] = useState("");
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    const storageTodoList = localStorage.getItem(TODO_APP_STORAGE_KEY);
+    if (storageTodoList) {
+      setItems(JSON.parse(storageTodoList));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem(TODO_APP_STORAGE_KEY, JSON.stringify(items));
+  }, [items]);
+
+  // change input value
+  const changeInputValue = useCallback((e) => {
+    setNewAct(e.target.value);
+  }, []);
+
+  // helper functions
+
+  const addActivity = useCallback(() => {
+    const newItemObject = {
+      id: Math.floor(Math.random() * 100),
+      value: newAct,
+    };
+    setItems([newItemObject, ...items]);
+    setNewAct("");
+  }, [newAct, items]);
+
+  const deleteItem = useCallback(
+    (id) => {
+      const newArray = items.filter((item) => item.id !== id);
+      setItems(newArray);
+    },
+    [items]
+  );
+
+  const crossItem = useCallback((id) => {
+    const crossedItem = document.getElementById(`${id}`);
+    crossedItem.classList.toggle("crossed");
+  }, []);
+  // local storage
+
+  // react will render this
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="content">
+      <h3>Make Every Day a Better Day</h3>
+      {/* input form */}
+      <div className="form">
+        <input
+          type="text"
+          placeholder="What's your priority?"
+          value={newAct}
+          onChange={changeInputValue}
+        />
+        <button
+          className="add"
+          disabled={!newAct ? true : false}
+          onClick={addActivity}
         >
-          Learn React
-        </a>
-      </header>
+          Add
+        </button>
+      </div>
+      {/* list of activities */}
+      {items.map((item) => (
+        <li key={item.id} id={item.id}>
+          {item.value}
+
+          <button
+            className="delete"
+            onClick={() => {
+              deleteItem(item.id);
+            }}
+          >
+            <i class="fa-solid fa-trash-can"></i>
+          </button>
+          <button
+            className="tick"
+            onClick={() => {
+              crossItem(item.id);
+            }}
+          >
+            <i class="fa-solid fa-check-double"></i>
+          </button>
+        </li>
+      ))}
     </div>
   );
 }
